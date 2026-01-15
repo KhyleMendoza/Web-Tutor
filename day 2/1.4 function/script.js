19e4c3abffc63a4886c899cee33e11df3e485ebc
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-01-15 15:35:24 +0800

    Function

--- day 2/1.4 function/script.js
+++ day 2/1.4 function/script.js
@@ -1,5 +1,31 @@
 function Greet() {
     console.log("Hello!")
 }
-
 Greet()
+
+balance = 0
+input = 3
+
+if (input == 1) {
+    Add()
+} else if(input == 2) {4
+    Sub()
+} else {
+    console.log("Invalid Input!")
+}
+
+function Add() {
+    balance +=1;
+    console.log("Added 1$ to your balance!");
+    CheckBalance()
+}
+
+function Sub() {
+    balance -=1;
+    console.log("Subtracted 1$ to your balance!")
+    CheckBalance()
+}
+
+function CheckBalance() {
+    console.log(`Your balance is ${balance}$`);
+}
