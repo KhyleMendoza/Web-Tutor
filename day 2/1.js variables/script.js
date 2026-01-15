70815204706d01d45e6236b1843cb375c563667d
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-01-15 14:01:38 +0800

    Tutoring in Web Dev

---
+++ day 2/1.js variables/script.js
@@ -0,0 +1,34 @@
+//this is comment
+//variables
+let Name = "khyle"
+//print
+console.log(Name)
+
+let Age = 22
+console.log(Age)
+
+console.log("Your name is " + Name + " and your age is " + Age);
+
+console.log(`Your name is ${Name}, and your age is ${Age} !`);
+
+console.log("I will change your name into khyle2");
+
+//we can change the value of variable by doing this
+Name = "Khyle2"
+
+console.log(`Your new name is ${Name}`);
+
+const pi = 3.14
+
+console.log (`The value of pi is ${pi}!`);
+
+console.log("since the pie is const ican't change const value!");
+
+var money = 50
+
+console.log(`Your money is $${money}`)
+
+money -= 5
+console.log(`You bought a apple for 5$`)
+
+console.log(`Your money is $${money}`)
---
+++ day 2/2. javascript linking/script.js
@@ -0,0 +1,5 @@
+const submitBtn = document.getElementById('submit3');
+
+submitBtn.addEventListener('click', function() {
+    alert('lol');
+})
